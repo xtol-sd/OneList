0993a3ff6062300355f30d902f4e8ff1ece87b25
leeknowlton <aeriath@gmail.com>
2013-11-13 23:46:21 +0900

    added basic recipe, ingredient, and recipe_ingredient models and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Onelist::Application.routes.draw do
+  get "ingredients/index"
+  get "ingredients/new"
+  get "ingredients/edit"
+  get "recipes/index"
+  get "recipes/new"
+  get "recipes/edit"
   root :to => "home#index"
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
