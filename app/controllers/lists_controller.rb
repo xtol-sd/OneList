a4ca23edcff75eed38544ce348a7fde8ad3bd158
Crystal08 <Crystal08@compuserve.com>
2013-11-29 14:00:05 -0600

    Fixes bug of tab 2 overriding tab 1's items, by passing expanded params to update action in list controller. Expanded params consists of previously selected items and current selected items both- so items from tab 1 and from tab 2.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -34,9 +34,9 @@ class ListsController < ApplicationController
 
   def update
     @list = List.find(params[:id])
-    #raise params.inspect
+    # raise params.inspect
     if params[:add_items_button]
-      @list.update(list_params)
+      @list.update(expanded_list_params(@list))
       flash[:notice] = "Step 2 complete: Items chosen!" 
       redirect_to edit_list_path(@list)
     elsif @list.update(list_params)
@@ -54,6 +54,23 @@ class ListsController < ApplicationController
   end
 
   private
+    
+    def expanded_list_params(list) #list is @list from update action above
+        former_items = ListItem.where(:list_id => list.id)
+        former_items_ids = former_items.map {|list_item| list_item.item_id.to_s}
+        params[:list][:selected_items] = params[:list][:selected_items] + former_items_ids
+        params.require(:list).permit!
+    end 
+
+# {"utf8"=>"✓", "_method"=>"patch", 
+# "authenticity_token"=>"Cc207N8Aq8DfXUzorjCTii0eGaPIB4uqdByu8A2xH4k=", 
+# "list"=>{"selected_items"=>["", "3", "4", "5"]}, 
+# "add_items_button"=>"", 
+# "action"=>"update", "controller"=>"lists", 
+# "id"=>"26"}
+
+      #params from add_items, tab 2: params[:list]
+   
     def list_params
       params.require(:list).permit!
       #Update to correct permission once controller complete:
