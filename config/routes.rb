aa40190c91df34575457b95908875621e9664ca5
leeknowlton <aeriath@gmail.com>
2013-12-03 00:02:38 +0900

    added front page and fixed routes for it

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Onelist::Application.routes.draw do
- get "lists/:id/add_items" => "lists#add_items", as: "add_items"
 
- root :to => "lists#index"
+  get "lists/:id/add_items" => "lists#add_items", as: "add_items"
+  authenticated :user do
+    root :to => "lists#index", as: :authenticated_root
+  end
+
   devise_for :users, :controllers => {:registrations => "registrations"}
+
+  devise_scope :user do
+    root to: "home#index"
+  end
+
   resources :users
   resources :recipes, :items, :lists, :menus
 end
